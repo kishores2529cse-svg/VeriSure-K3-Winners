41c3adbffe1c34f549b4bb59aea99db988fa8009
Kalaisk-47 <kalaisk47@gmail.com>
2026-09-20 00:32:02 +0530

    upi guard removed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardLayout from "./components/layout/DashboardLayout";
 import HomeDashboard from "./pages/HomeDashboard";
-import UpiGuard from "./pages/UpiGuard";
 import ScamScanner from "./pages/ScamScanner";
 import ProctorVision from "./pages/ProctorVision";
 import ScamLists from "./pages/ScamLists";
@@ -12,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<DashboardLayout />}>
           <Route index element={<HomeDashboard />} />
-          <Route path="upi" element={<UpiGuard />} />
           <Route path="scam-scanner" element={<ScamScanner />} />
           <Route path="proctor" element={<ProctorVision />} />
           <Route path="scam-lists" element={<ScamLists />} />
--- frontend/src/components/layout/DashboardLayout.tsx
+++ frontend/src/components/layout/DashboardLayout.tsx
@@ -4,7 +4,6 @@ import FloatingLines from "../visuals/FloatingLines";
 
 const navigation = [
   { name: "Dashboard", href: "/", icon: LayoutDashboard },
-  { name: "UPI Guard", href: "/upi", icon: ShieldAlert },
   { name: "Scam Scanner", href: "/scam-scanner", icon: Bug },
   { name: "Proctor Vision", href: "/proctor", icon: Camera },
   { name: "Scam Lists", href: "/scam-lists", icon: BookOpen },
